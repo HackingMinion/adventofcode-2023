e089f24e9cfe84a96f437bbf8bcea99272aaef7b
Michel Gabriel <mitch15@proton.me>
2023-12-03 22:56:50 +0100

    feat: add part1 of day 1 challenge

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,11 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val listOfNumbers = mutableListOf<Int>();
+        for (line in input) {
+            val digits = line.toCharArray().filter { it.isDigit()}
+            listOfNumbers.add("${digits[0]}${digits[digits.count()-1]}".toInt())
+        }
+        return listOfNumbers.sum();
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +14,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 137)
 
     val input = readInput("Day01")
     part1(input).println()
