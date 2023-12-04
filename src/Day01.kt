275d9c6ce202bd6d5246d6bb1df8a5df39e5e63b
Michel Gabriel <mitch15@proton.me>
2023-12-04 22:24:30 +0100

    feat: finish part2 of day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -24,30 +24,41 @@ fun main() {
         return listOfNumbers.sum();
     }
 
+    fun getStringIndex(line: String, number: Number, index: Int = 0): Int {
+        return line.indexOf(number.name.lowercase(Locale.getDefault()), index)
+    }
+
+    fun getDigitIndex(line: String, number: Number, index: Int = 0): Int {
+        return line.indexOf(number.value.toString(), index)
+    }
+
     fun part2(input: List<String>): Int {
+        val listOfNumbers = mutableListOf<Int>();
         for (line in input) {
             val listOfDigits = mutableListOf<Digits>();
             for (number in Number.entries) {
-                val stringIndex = line.indexOf(number.name.lowercase(Locale.getDefault()))
-                if(stringIndex != -1) {
+                var stringIndex = getStringIndex(line, number)
+                while(stringIndex != -1) {
                     listOfDigits.add(Digits(number.value, stringIndex))
+                    stringIndex = getStringIndex(line, number, stringIndex+1)
                 }
 
-                val numberIndex = line.indexOf(number.value.toString())
-                if(numberIndex != -1) {
+                var numberIndex = getDigitIndex(line, number)
+                while(numberIndex != -1) {
                     listOfDigits.add(Digits(number.value, numberIndex))
+                    numberIndex = getDigitIndex(line, number, numberIndex+1)
                 }
             }
             listOfDigits.sortBy { it.index }
-            println(listOfDigits.map{it.number})
+            listOfNumbers.add("${listOfDigits[0].number}${listOfDigits[listOfDigits.count()-1].number}".toInt())
         }
-        return 1;
+        return listOfNumbers.sum();
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 137)
-    check(part2(testInput) == 0)
+    check(part2(testInput) == 67)
 
     val input = readInput("Day01")
     part1(input).println()
